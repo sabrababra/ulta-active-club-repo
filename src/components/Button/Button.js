3b2b1f99c439f862d9730f080eb23ebc5ce4387e
sabrababra <sabrinayesminprome@gmail.com>
2022-09-29 03:39:00 +0600

    Activity button added

---
+++ src/components/Button/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Button.css';
+const Button = () => {
+    return (
+        <div className='mt-5'>
+             <button class="btn btn-primary w-100" type="button">Activity Completed</button>
+        </div>
+    );
+};
+
+export default Button;
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -3,12 +3,14 @@ import './Sidebar.css';
 import UserInfo from '../UserInfo/UserInfo';
 import Break from '../Break/Break';
 import Detail from '../Detail/Detail';
+import Button from '../Button/Button';
 const Sidebar = ({data}) => {
     return (
         <div className='p-3 sitebar-container'>
             <UserInfo></UserInfo>
             <Break></Break>
             <Detail data={data}></Detail>
+            <Button></Button>
             
          
         </div>
