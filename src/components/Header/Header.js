f3326ff1cad9cd6847f1ae41b2100b2bfdc0411c
sabrababra <sabrinayesminprome@gmail.com>
2022-09-28 22:05:52 +0600

    Header Complete

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        
+        <nav className='navbar '>
+        <div className='container-fluid'>
+          <a className='navbar-brand' href="#">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Book-icon-bible.png/800px-Book-icon-bible.png" alt="Logo" width="60" height="40" className='d-inline-block align-text-top'/>
+            <span className='fs-4'>Read Book</span>
+            <h4>Reading book regularly</h4>
+          </a>
+        </div>
+      </nav>
+        
+    );
+};
+
+export default Header;
