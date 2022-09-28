4184ba8060e2e5149077a4d41c042dabc2f1709b
sabrababra <sabrinayesminprome@gmail.com>
2022-09-29 00:40:38 +0600

    Load data from Api and show in UI

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Sidebar.css'
+const Sidebar = () => {
+    return (
+        <div>
+            <h4>This is sidebar</h4>
+            <img className='protfolio-img' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfvcw2EqspliOB3odOlleO_KfqNy4No1Q-8nSmqt4&s" alt="" />
+        </div>
+    );
+};
+
+export default Sidebar;
